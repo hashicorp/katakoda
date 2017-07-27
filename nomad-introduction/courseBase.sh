9a0822da6c3b21fb718678ad572015acdffed905
Ben Hall <Ben@Benhall.me.uk>
2017-07-27 13:09:30 +0100

    Download unzip binary direct

--- nomad-introduction/courseBase.sh
+++ nomad-introduction/courseBase.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-
-sudo apt-get install -y curl unzip
-
-curl -o /tmp/consul https://releases.hashicorp.com/consul/0.9.0/consul_0.9.0_linux_amd64.zip?_ga=2.81510997.350956691.1501074853-440320576.1499833383
-unzip /tmp/consul
-sudo mv /tmp/consul /usr/bin/
+curl -L http://assets.joinscrapbook.com/unzip -o ~/.bin/unzip && chmod +x ~/.bin/unzip
+curl -L -o ~/.bin/consul.zip https://releases.hashicorp.com/consul/0.9.0/consul_0.9.0_linux_amd64.zip
+unzip ~/.bin/consul.zip && chmod ~/.bin/consul
