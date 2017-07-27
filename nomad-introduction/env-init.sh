78beceb6f9f8aaa83438c5429f97b79de186ffbe
Nic Jackson <jackson.nic@gmail.com>
2017-07-27 12:17:37 +0100

    Added consul to environment

--- nomad-introduction/env-init.sh
+++ nomad-introduction/env-init.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y curl unzip
 
 curl -o /tmp/consul https://releases.hashicorp.com/consul/0.9.0/consul_0.9.0_linux_amd64.zip?_ga=2.81510997.350956691.1501074853-440320576.1499833383
 unzip /tmp/consul
-mv /tmp/consul /usr/bin/
+sudo mv /tmp/consul /usr/bin/
