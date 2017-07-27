780f5c85c64703b5bf8245fe7c565e208f2568d2
Nic Jackson <jackson.nic@gmail.com>
2017-07-27 13:16:20 +0100

    Updated

--- nomad-introduction/courseBase.sh
+++ nomad-introduction/courseBase.sh
@@ -1,3 +1,3 @@
 curl -L http://assets.joinscrapbook.com/unzip -o ~/.bin/unzip && chmod +x ~/.bin/unzip
 curl -L -o ~/.bin/consul.zip https://releases.hashicorp.com/consul/0.9.0/consul_0.9.0_linux_amd64.zip
-unzip ~/.bin/consul.zip && chmod +x ~/.bin/consul
+unzip -d ~/.bin ~/.bin/consul.zip && chmod +x ~/.bin/consul
