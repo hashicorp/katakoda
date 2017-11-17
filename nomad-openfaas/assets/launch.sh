29c4fd36ff8582f13f7c2e061495438774c62f3e
Ben Hall <ben@benhall.me.uk>
2017-11-17 15:42:26 +0000

    Provide user feedback when starting Nomad/OpenFaaS

---
+++ nomad-openfaas/assets/launch.sh
@@ -0,0 +1,18 @@
+echo "Starting Nomad and OpenFaaS"
+echo "Waiting for Nomad to start... This may take a couple of moments"
+n=0
+until [ $n -ge 10 ]
+do
+  response=`curl -sL -w "%{http_code}\\n" "http://host01:4646/v1/status/leader" -o /dev/null --connect-timeout 3 --max-time 5`
+  if [[ "${response}" == "200" ]]; then
+    echo "NOMAD Running"
+    break
+  fi
+
+  n=$[$n+1]
+  sleep 2
+done
+echo "Nomad started. "
+echo -n "Configuring... "
+nomad run ~/faas.hcl
+echo "Nomad with OpenFaaS Ready"
