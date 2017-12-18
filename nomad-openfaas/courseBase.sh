9ff3b08fcbabef67a9bde9b5571a4f421ffe7569
Nic Jackson <jackson.nic@gmail.com>
2017-12-18 15:50:31 +0000

    Updated openfaas tutorial to add local registry

--- nomad-openfaas/courseBase.sh
+++ nomad-openfaas/courseBase.sh
@@ -22,3 +22,12 @@ chmod +x ~/.bin/faas-cli
 rm ~/nomad.zip ~/consul.zip ~/terraform.zip
 
 apt-get install -y tree
+
+# Setup Docker registry
+docker run -d -e VIRTUAL_HOST=registry.test.training.katacoda.com -v /opt/registry/data:/var/lib/registry --name registry registry:2
+
+docker create -v /etc/nginx/certs --name nginx_certs busybox
+docker cp /certs/registry.test.training.katacoda.com.crt nginx_certs:/etc/nginx/certs/
+docker cp /certs/registry.test.training.katacoda.com.key nginx_certs:/etc/nginx/certs/
+
+docker run -d -p 80:80 -p 443:443 --volumes-from nginx_certs -v /var/run/docker.sock:/tmp/docker.sock:ro --name nginx benhall/nginx-registry-proxy:1.9.6
--- nomad-openfaas/terminal.sh
+++ nomad-openfaas/terminal.sh
@@ -1,12 +1,3 @@
 export NOMAD_ADDR=http://host01:4646
 
-# Setup Docker registry
-docker run -d -e VIRTUAL_HOST=registry.test.training.katacoda.com -v /opt/registry/data:/var/lib/registry --name registry registry:2
-
-docker create -v /etc/nginx/certs --name nginx_certs busybox
-docker cp /certs/registry.test.training.katacoda.com.crt nginx_certs:/etc/nginx/certs/
-docker cp /certs/registry.test.training.katacoda.com.key nginx_certs:/etc/nginx/certs/
-
-docker run -d -p 80:80 -p 443:443 --volumes-from nginx_certs -v /var/run/docker.sock:/tmp/docker.sock:ro --name nginx benhall/nginx-registry-proxy:1.9.6
-
 sleep 1; ~/launch.sh;
