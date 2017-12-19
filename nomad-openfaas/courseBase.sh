69daae682316556ab4644f6ab769d993eea2ccea
Nic Jackson <jackson.nic@gmail.com>
2017-12-19 07:44:38 +0000

    Updated registry

--- nomad-openfaas/courseBase.sh
+++ nomad-openfaas/courseBase.sh
@@ -21,15 +21,8 @@ chmod +x ~/.bin/faas-cli
 
 rm ~/nomad.zip ~/consul.zip ~/terraform.zip
 
+# Install additional packages
 apt-get install -y tree
 
 # Setup Docker registry
-docker run -d -e VIRTUAL_HOST=registry.test.training.katacoda.com -v /opt/registry/data:/var/lib/registry --name registry registry:2
-
-docker create -v /etc/nginx/certs --name nginx_certs busybox
-docker cp /certs/registry.test.training.katacoda.com.crt nginx_certs:/etc/nginx/certs/
-docker cp /certs/registry.test.training.katacoda.com.key nginx_certs:/etc/nginx/certs/
-
-docker run -d -p 80:80 -p 443:443 --volumes-from nginx_certs -v /var/run/docker.sock:/tmp/docker.sock:ro --name nginx benhall/nginx-registry-proxy:1.9.6
-
-sed -i "/registry.test.training.katacoda.com/ s/.*/$(cat /etc/hosts | grep host01 | grep -Eo '([0-9]*\.){3}[0-9]*')\tregistry.test.training.katacoda.com/g" /etc/hosts
+docker run -d --name registry -p 5000:5000 registry:2
