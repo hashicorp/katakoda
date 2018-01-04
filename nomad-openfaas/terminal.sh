e498ef16d1a0ed75b23b9312c8f84bb91f7e2696
Nic Jackson <jackson.nic@gmail.com>
2018-01-04 14:38:19 +0000

    Updated terminal command

--- nomad-openfaas/terminal.sh
+++ nomad-openfaas/terminal.sh
@@ -4,5 +4,5 @@ export NOMAD_ADDR=http://host01:4646
 sleep 1; ~/launch.sh;
 
 sleep 1; clear
-# Docker Registry URL: https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2
-# OpenFaaS Gateway: https://[[HOST_SUBDOMAIN]]-8078-[[KATACODA_HOST]].environments.katacoda.com/
+: The Docker Registry URL is https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2
+: The OpenFaaS Gateway is https://[[HOST_SUBDOMAIN]]-8078-[[KATACODA_HOST]].environments.katacoda.com/
