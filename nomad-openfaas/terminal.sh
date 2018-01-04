a9d80effa3f5ac4182ed42e4802e0ffbed7ba403
Nic Jackson <jackson.nic@gmail.com>
2018-01-04 13:39:53 +0000

    Updated terminal.sh

--- nomad-openfaas/terminal.sh
+++ nomad-openfaas/terminal.sh
@@ -2,3 +2,6 @@ export NOMAD_ADDR=http://host01:4646
 
 # Start Nomad
 sleep 1; ~/launch.sh;
+
+echo "DOCKER REGISTRY URL: https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2"
+echo "OpenFaaS GATEWAY: https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com/"
