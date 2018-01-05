b34195ab5967d20a91fa860f5f22c560fe6359fb
Nic Jackson <jackson.nic@gmail.com>
2018-01-05 14:27:45 +0000

    Updated terminal settings:

--- nomad-openfaas/terminal.sh
+++ nomad-openfaas/terminal.sh
@@ -1,7 +1,12 @@
 export NOMAD_ADDR=http://host01:4646
+export FAAS_GATEWAY=https://[[HOST_SUBDOMAIN]]-8078-[[KATACODA_HOST]].environments.katacoda.com/
+export DOCKER_REGISTRY=https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2
 
 # Start Nomad
 sleep 1; ~/launch.sh;
+
+# Install templates
+faas-cli template pull
 
 sleep 1; clear
 : The Docker Registry URL is https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2
