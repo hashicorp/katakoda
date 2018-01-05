e98b7a8e6d3acf2c10df85e3f84b87be12c18cd0
Nic Jackson <jackson.nic@gmail.com>
2018-01-05 15:41:11 +0000

    Updated terminal

--- nomad-openfaas/terminal.sh
+++ nomad-openfaas/terminal.sh
@@ -8,6 +8,6 @@ sleep 1; ~/launch.sh;
 # Install templates
 faas-cli template pull
 
-#sleep 1; clear
+clear
 : The Docker Registry URL is https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2
 : The OpenFaaS Gateway is https://[[HOST_SUBDOMAIN]]-8078-[[KATACODA_HOST]].environments.katacoda.com/
