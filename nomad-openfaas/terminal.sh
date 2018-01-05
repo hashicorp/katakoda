4b92468f9fb0e16f7a26af053e9ef0232d4519dc
Nic Jackson <jackson.nic@gmail.com>
2018-01-05 15:55:36 +0000

    Updated terminal

--- nomad-openfaas/terminal.sh
+++ nomad-openfaas/terminal.sh
@@ -1,6 +1,7 @@
 export NOMAD_ADDR=http://host01:4646
-export FAAS_GATEWAY=https://[[HOST_SUBDOMAIN]]-8078-[[KATACODA_HOST]].environments.katacoda.com/
-export DOCKER_REGISTRY=https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2
+export FAAS_GATEWAY=https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com/
+export DOCKER_REGISTRY_URL=https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2
+export DOCKER_REGISTRY=[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com
 
 # Start Nomad
 sleep 1; ~/launch.sh;
@@ -11,5 +12,5 @@ clear
 faas-cli template pull
 
 # Echo status
-: The Docker Registry URL is https://[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/v2
-: The OpenFaaS Gateway is https://[[HOST_SUBDOMAIN]]-8078-[[KATACODA_HOST]].environments.katacoda.com/
+: The local Docker Registry is [[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com
+: The OpenFaaS Gateway is https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com/
