a87b8300a3dcf20f980b959a5fbdb301a81b9a20
Yoko Hyakuna <yoko@hashicorp.com>
2019-06-11 11:51:25 -0700

    Updated

--- vault-tools/env-init.sh
+++ vault-tools/env-init.sh
@@ -2,7 +2,7 @@
 curl -s -L http://assets.joinscrapbook.com/unzip -o ~/.bin/unzip
 chmod +x ~/.bin/unzip
 
-curl -s -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.1.2/vault_1.1.2_linux_amd64.zip &&
+curl -s -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.1.3/vault_1.1.3_linux_amd64.zip &&
 unzip -d ~/.bin ~/vault.zip && rm ~/vault.zip
 
 curl -sLo ~/consul-template.zip https://releases.hashicorp.com/consul-template/0.20.0/consul-template_0.20.0_linux_amd64.zip &&
@@ -15,4 +15,4 @@ export VAULT_ADDR='http://127.0.0.1:8200'
 
 apt-get install -y jq
 
-ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.1.2/vault_1.1.2_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip && apt-get install -y jq &$ export VAULT_ADDR='http://127.0.0.1:8200' && curl -sLo ~/consul-template.zip https://releases.hashicorp.com/consul-template/0.20.0/consul-template_0.20.0_linux_amd64.zip && unzip -d ~/.bin ~/consul-template.zip && rm ~/consul-template.zip && curl -sLo ~/envconsul.zip https://releases.hashicorp.com/envconsul/0.7.3/envconsul_0.7.3_linux_amd64.zip && unzip -d ~/.bin ~/envconsul.zip && rm ~/envconsul.zip &"
+ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.1.3/vault_1.1.3_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip && apt-get install -y jq &$ export VAULT_ADDR='http://127.0.0.1:8200' && curl -sLo ~/consul-template.zip https://releases.hashicorp.com/consul-template/0.20.0/consul-template_0.20.0_linux_amd64.zip && unzip -d ~/.bin ~/consul-template.zip && rm ~/consul-template.zip && curl -sLo ~/envconsul.zip https://releases.hashicorp.com/envconsul/0.7.3/envconsul_0.7.3_linux_amd64.zip && unzip -d ~/.bin ~/envconsul.zip && rm ~/envconsul.zip &"
