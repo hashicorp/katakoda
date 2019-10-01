ecd0ef66305dd97fb52f0604bf9886e0a567640b
Rodrigo DL <ramakandra@gmail.com>
2019-10-01 11:05:13 +0200

    fixed nomad agent start on host01

--- nomad-openfaas/assets/install.sh
+++ nomad-openfaas/assets/install.sh
@@ -15,5 +15,7 @@ rm ~/nomad.zip ~/consul.zip
 mkdir -p ~/log
 # TODO: Is this the correct public IP address?
 PUBLIC_IP=$(ip route get 1 | cut -d ' ' -f 7)
+IF=$(ip route get 1 | head -n1 | cut -d ' ' -f 5)
 nohup consul agent -dev -client="$PUBLIC_IP 127.0.0.1" -advertise=$PUBLIC_IP >~/log/consul.log 2>&1 &
-nohup nomad agent -dev -network-interface=ens3 -bind=0.0.0.0 >~/log/nomad.log 2>&1 &
+
+nohup nomad agent -dev -network-interface=${IF} -bind=0.0.0.0 >~/log/nomad.log 2>&1 &
