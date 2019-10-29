e2c700aa95f953e73d4d737b891bca93d029bf4c
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2019-10-28 17:36:55 -0700

    Use hashicorp-hashistack disk image for faster boot

--- nomad-introduction/courseBase.sh
+++ nomad-introduction/courseBase.sh
@@ -1,24 +1,7 @@
-curl -L http://assets.joinscrapbook.com/unzip -o ~/.bin/unzip
-chmod +x ~/.bin/unzip
-
-curl -L -o ~/consul.zip https://releases.hashicorp.com/consul/1.2.2/consul_1.2.2_linux_amd64.zip
-unzip -d  ~/.bin/ ~/consul.zip
-chmod +x ~/.bin/consul
-
-curl -L -o ~/nomad.zip https://releases.hashicorp.com/nomad/0.8.4/nomad_0.8.4_linux_amd64.zip
-unzip -d  ~/.bin/ ~/nomad.zip
-chmod +x  ~/.bin/nomad
-
-rm ~/nomad.zip ~/consul.zip
-
 export NOMAD_ADDR="http://host01:4646"
 
-host01_commands=(
-"curl --location https://raw.githubusercontent.com/hashicorp/katakoda/master/nomad-introduction/assets/host01_install.sh --remote-name"
-"sh host01_install.sh"
-)
+mkdir -p ~/log
+nohup sh -c "consul agent -dev >~/log/consul.log 2>&1" > ~/log/nohup.log &
+nohup sh -c "nomad agent -dev -bind=0.0.0.0 >~/log/nomad.log 2>&1" > ~/log/nohup.log &
 
-all_commands=$(awk -v sep=' && ' 'BEGIN{ORS=OFS="";for(i=1;i<ARGC;i++){print ARGV[i],ARGC-i-1?sep:""}}' "${host01_commands[@]}")
-
-echo "$all_commands"
-ssh root@host01 "$all_commands"
+nomad -autocomplete-install
