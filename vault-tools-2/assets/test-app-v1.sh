956f451812a04c96c712409bcebf64648055d36d
Yoko Hyakuna <yoko@hashicorp.com>
2019-10-31 09:45:13 -0700

    Updated

--- vault-tools-2/assets/test-app-v1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cat <<EOT
-Customer info is:
-
-  Organization: ${KV_V1_CUSTOMERS_ACME_ORGANIZATION}
-  ID: ${KV_V1_CUSTOMERS_ACME_CUSTOMER_ID}
-  Contact: ${KV_V1_CUSTOMERS_ACME_CONTACT_EMAIL}
-EOT
