1fe8d1fd9d9d68d334fd8fd4cbecf7df701b1d06
Zack Smith <acidprime@wallcity.org>
2020-01-14 15:14:42 -0800

    Update Vault to 1.3.1
    Prior to this commit vault was installed as 1.3.0 , this commit updates
    the version per [this request](https://hashicorp.slack.com/archives/CA3ES0VHA/p1579034935010800?thread_ts=1579031819.006500&cid=CA3ES0VHA)

--- environments/hashistack/build/1_hashicorp_tools.sh
+++ environments/hashistack/build/1_hashicorp_tools.sh
@@ -22,7 +22,7 @@ install_zip "nomad" "https://releases.hashicorp.com/nomad/0.10.1/nomad_0.10.1_li
 
 install_zip "terraform" "https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip"
 
-install_zip "vault" "https://releases.hashicorp.com/vault/1.3.0/vault_1.3.0_linux_amd64.zip"
+install_zip "vault" "https://releases.hashicorp.com/vault/1.3.1/vault_1.3.1_linux_amd64.zip"
 
 install_zip "consul-template" "https://releases.hashicorp.com/consul-template/0.22.1/consul-template_0.22.1_linux_amd64.zip"
 
