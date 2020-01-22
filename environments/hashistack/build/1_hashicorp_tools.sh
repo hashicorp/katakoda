ec7d41981c749db000cc6cf9199f6e0f9d8ef2cc
Yoko Hyakuna <yoko@hashicorp.com>
2020-01-22 11:35:00 -0800

    Updated the version of Vault, Consul-Template, and Envconsul

--- environments/hashistack/build/1_hashicorp_tools.sh
+++ environments/hashistack/build/1_hashicorp_tools.sh
@@ -22,8 +22,8 @@ install_zip "nomad" "https://releases.hashicorp.com/nomad/0.10.1/nomad_0.10.1_li
 
 install_zip "terraform" "https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip"
 
-install_zip "vault" "https://releases.hashicorp.com/vault/1.3.1/vault_1.3.1_linux_amd64.zip"
+install_zip "vault" "https://releases.hashicorp.com/vault/1.3.2/vault_1.3.2_linux_amd64.zip"
 
-install_zip "consul-template" "https://releases.hashicorp.com/consul-template/0.22.1/consul-template_0.22.1_linux_amd64.zip"
+install_zip "consul-template" "https://releases.hashicorp.com/consul-template/0.24.0/consul-template_0.24.0_linux_amd64.zip"
 
-install_zip "envconsul" "https://releases.hashicorp.com/envconsul/0.9.1/envconsul_0.9.1_linux_amd64.zip"
+install_zip "envconsul" "https://releases.hashicorp.com/envconsul/0.9.2/envconsul_0.9.2_linux_amd64.zip"
