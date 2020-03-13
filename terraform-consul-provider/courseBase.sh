626d4125e9563d7ee2b56ba218d602a885b06236
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-03-13 10:06:00 -0700

    update katacoda

--- terraform-consul-provider/courseBase.sh
+++ terraform-consul-provider/courseBase.sh
@@ -1,15 +1,18 @@
 # Install Terraform and init config
+# Install docker - Ubuntu doesn't have docker
+apt-get install docker-ce docker-ce-cli containerd.io
+
 # Install unzip - Katacoda docker image doesn't have unzip
 apt-get install unzip
-cd ~
+cd ..
 curl -O https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
 unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
-export HOME=~/tutorial
-cd $HOME
 
 # Clone docker compose files
-git clone https://github.com/hashicorp/getting-started-terraform-consul-provider .
-cd consul-playground 
+git clone https://github.com/hashicorp/getting-started-terraform-consul-provider
+mv getting-started-terraform-consul-provider/* ~
+
+cd ~/consul-playground 
 
 # Run Docker Compose up (daemon)
 docker-compose up -d
