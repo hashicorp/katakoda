9d8d102b5c7a404a8e65dbd63aab44b395e0c8f6
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2020-04-15 12:12:14 -0400

    Fix typo

--- nomad-connect-acls/provision.courseData.sh
+++ nomad-connect-acls/provision.courseData.sh
@@ -188,7 +188,7 @@ install_cni() {
   curl -s -L -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.8.4/cni-plugins-linux-amd64-v0.8.4.tgz
   sudo mkdir -p /opt/cni/bin
   sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
-  rm cni-pluginz.tgz
+  rm cni-plugins.tgz
 }
 
 finish() {
