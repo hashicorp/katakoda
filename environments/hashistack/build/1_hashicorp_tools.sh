39b654f61beef86d5fb55fec1b5cab3fba2c609a
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2020-06-04 18:01:56 -0700

    Update version numbers of all tools

--- environments/hashistack/build/1_hashicorp_tools.sh
+++ environments/hashistack/build/1_hashicorp_tools.sh
@@ -16,18 +16,18 @@ install_zip()
     rm ~/$NAME.zip
 }
 
-install_zip "consul" "https://releases.hashicorp.com/consul/1.7.2/consul_1.7.2_linux_amd64.zip"
+install_zip "consul" "https://releases.hashicorp.com/consul/1.7.3/consul_1.7.3_linux_amd64.zip"
 
-install_zip "nomad" "https://releases.hashicorp.com/nomad/0.10.4/nomad_0.10.4_linux_amd64.zip"
+install_zip "nomad" "https://releases.hashicorp.com/nomad/0.11.2/nomad_0.11.2_linux_amd64.zip"
 
-install_zip "terraform" "https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip"
+install_zip "terraform" "https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip"
 
 install_zip "vault" "https://releases.hashicorp.com/vault/1.4.2/vault_1.4.2_linux_amd64.zip"
 
-install_zip "consul-template" "https://releases.hashicorp.com/consul-template/0.24.0/consul-template_0.24.0_linux_amd64.zip"
+install_zip "consul-template" "https://releases.hashicorp.com/consul-template/0.25.0/consul-template_0.25.0_linux_amd64.zip"
 
-install_zip "envconsul" "https://releases.hashicorp.com/envconsul/0.9.2/envconsul_0.9.2_linux_amd64.zip"
+install_zip "envconsul" "https://releases.hashicorp.com/envconsul/0.9.3/envconsul_0.9.3_linux_amd64.zip"
 
-install_zip "sentinel" "https://releases.hashicorp.com/sentinel/0.14.3/sentinel_0.14.3_linux_amd64.zip"
+install_zip "sentinel" "https://releases.hashicorp.com/sentinel/0.15.5/sentinel_0.15.5_linux_amd64.zip"
 
-install_zip "packer" "https://releases.hashicorp.com/packer/1.5.1/packer_1.5.1_linux_amd64.zip"
+install_zip "packer" "https://releases.hashicorp.com/packer/1.5.6/packer_1.5.6_linux_amd64.zip"
