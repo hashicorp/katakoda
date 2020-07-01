2e361aba736ec34fe35c200444a94de3b6a445de
Yoko <yoko@hashicorp.com>
2020-07-01 14:33:27 -0700

    Update to 0.12 syntax

--- vault-terraform/assets/main.tf
+++ vault-terraform/assets/main.tf
@@ -1,43 +1,44 @@
 # Use Vault provider
-provider "vault" { }
+provider "vault" {
+}
 
 # Create a policy document
 data "vault_policy_document" "training" {
-    rule {
-        path         = "kv-v2/data/training/*"
-        capabilities = ["create", "read", "update", "delete", "list"]
-    }
-    rule {
-        path         = "kv-v2/*"
-        capabilities = ["list", "read"]
-    }
-    rule {
-        path         = "transit/encrypt/payment"
-        capabilities = ["update"]
-    }
-    rule {
-        path         = "transit/decrypt/payment"
-        capabilities = ["update"]
-    }
-    rule {
-        path         = "transit/*"
-        capabilities = ["read", "list"]
-    }
+  rule {
+    path         = "kv-v2/data/training/*"
+    capabilities = ["create", "read", "update", "delete", "list"]
+  }
+  rule {
+    path         = "kv-v2/*"
+    capabilities = ["list", "read"]
+  }
+  rule {
+    path         = "transit/encrypt/payment"
+    capabilities = ["update"]
+  }
+  rule {
+    path         = "transit/decrypt/payment"
+    capabilities = ["update"]
+  }
+  rule {
+    path         = "transit/*"
+    capabilities = ["read", "list"]
+  }
 }
 
 # Create 'training' policy
 resource "vault_policy" "training" {
-    name   = "training"
-    policy = "${data.vault_policy_document.training.hcl}"
+  name   = "training"
+  policy = data.vault_policy_document.training.hcl
 }
 
 # Enable auth method
 resource "vault_auth_backend" "userpass" {
-     type = "userpass"
+  type = "userpass"
 }
 
 resource "vault_generic_endpoint" "student" {
-  depends_on           = ["vault_auth_backend.userpass"]
+  depends_on           = [vault_auth_backend.userpass]
   path                 = "auth/userpass/users/student"
   ignore_absent_fields = true
 
@@ -47,6 +48,7 @@ resource "vault_generic_endpoint" "student" {
   "password": "changeme"
 }
 EOT
+
 }
 
 # Enable K/V v2 secrets engine at 'kv-v2'
@@ -64,6 +66,6 @@ resource "vault_mount" "transit" {
 # Creating an encryption key named 'payment'
 resource "vault_transit_secret_backend_key" "key" {
   depends_on = [vault_mount.transit]
-  backend = "transit"
-  name    = "payment"
+  backend    = "transit"
+  name       = "payment"
 }
