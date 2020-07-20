5a32f657c3ca81e050344e74098066f312ef26e6
Yoko <yoko@hashicorp.com>
2020-07-20 14:01:34 -0700

    Add provider version requirement

--- vault-terraform-ent/assets/main.tf
+++ vault-terraform-ent/assets/main.tf
@@ -1,15 +1,11 @@
-#------------------------------------------------------------------------------
-# The best practice is to use remote state file and encrypt it since your
-# state files may contains sensitive data (secrets).
-#------------------------------------------------------------------------------
-# terraform {
-#       backend "s3" {
-#             bucket = "remote-terraform-state-dev"
-#             encrypt = true
-#             key = "terraform.tfstate"
-#             region = "us-east-1"
-#       }
-# }
+#-----------------------------------------------------------------------------------
+# To configure Transform secrets engine, you need vault provider v2.12.0 or later
+#-----------------------------------------------------------------------------------
+terraform {
+  required_providers {
+    vault = "~> 2.12"
+  }
+}
 
 #------------------------------------------------------------------------------
 # To leverate more than one namespace, define a vault provider per namespace
