af126b8d74e00b869f41c0c90571ab263977a6c6
Zack Smith <acidprime@wallcity.org>
2020-09-22 12:36:59 -0700

    Add IPC_LOCK check
    Prior to this commit the IPC_LOCK requirement on vault (as post
    installed in its package scripts) caused running vault to fail when this
    cap was not added to the running docker check. This commit adds a simple
    help command check which would have caught this eventuality

--- environments/hashistack/controls/vault_control.rb
+++ environments/hashistack/controls/vault_control.rb
@@ -8,4 +8,9 @@ control 'vault' do
     it { should be_installed }
   end
 
+  # Check for exit for IPC_LOCK in docker requirement
+  describe command('vault -h') do
+    its('exit_status') { should eq 0 }
+  end
+
 end
