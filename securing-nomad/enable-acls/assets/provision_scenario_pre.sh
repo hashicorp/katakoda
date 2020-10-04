567c3e8eb64e7ffce795166b4c96f3793d44a45c
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2020-10-04 17:36:25 -0400

    Reworking provisioner to handle course format

--- securing-nomad/enable-acls/assets/provision_scenario_pre.sh
+++ securing-nomad/enable-acls/assets/provision_scenario_pre.sh
@@ -1,4 +1,7 @@
+if [ ! -f ~/tls_environment ]
+then
 echo "Additional scenario specific provisioning..."
+if [ -d /opt/nomad/ca ] then; echo "Resetting TLS configuration"; rm -rf /opt/nomad/ca; fi
 
 mkdir -p /opt/nomad/ca
 cd /opt/nomad/ca
@@ -9,6 +12,13 @@ mkdir -p /etc/nomad.d/tls
 cp nomad-agent-ca.pem /etc/nomad.d/tls
 cp global-server-nomad-0* /etc/nomad.d/tls
 cp global-client-nomad-0* /etc/nomad.d/tls
+
+for I in {1..3}; do
+  sed "s/{{NODE}}/server$I/g" /tmp/server.hcl.template > /opt/nomad/server$I/nomad.hcl
+  restart-server$I
+done
+sed "s/{{NODE}}/client/g" /tmp/client.hcl.template > /opt/nomad/client/nomad.hcl
+restart-client
 
 cat > ~/tls_environment <<EOF
 echo "Preloading TLS Environment Variables..."
@@ -21,3 +31,5 @@ echo $ export NOMAD_CLIENT_CERT="/etc/nomad.d/tls/global-server-nomad-0.pem"
 echo $ export NOMAD_CLIENT_KEY="/etc/nomad.d/tls/global-server-nomad-0-key.pem"
 echo $ export NOMAD_ADDR="https://127.0.0.1:4646"
 EOF
+
+fi
--- securing-nomad/encrypt-gossip/assets/provision.sh
+++ securing-nomad/encrypt-gossip/assets/provision.sh
@@ -62,34 +62,40 @@ maybe_postprovision() {
   fi
 }
 
+provision_namespace() {
+  if [ ! -f /provision/namespace_done ]
+  then
+    while [ ! -x /usr/local/bin/provision_ns.sh ]
+    do 
+      sleep 1
+    done
+    /usr/local/bin/provision_ns.sh
+    touch /provision/namespace_done
+  fi
+}
+
 finish() {
-  touch /provision_complete
   log "Complete!  Move on to the next step."
 }
 
 provision() {
-  fix_journal
-  install_apt_deps
-  install_pyhcl
+  if [ ! -f /provision/provision_done ]
+  then
+    fix_journal
+    install_apt_deps
+    install_pyhcl
+    install_zip "consul" "https://releases.hashicorp.com/consul/1.8.4/consul_1.8.4_linux_amd64.zip"
+    install_zip "nomad" "https://releases.hashicorp.com/nomad/0.12.5/nomad_0.12.5_linux_amd64.zip"
 
-  install_zip "consul" "https://releases.hashicorp.com/consul/1.8.4/consul_1.8.4_linux_amd64.zip"
-  install_zip "nomad" "https://releases.hashicorp.com/nomad/0.12.5/nomad_0.12.5_linux_amd64.zip"
-
-  mkdir -p /etc/nomad.d /opt/nomad/data
-
-  maybe_preprovision
-
-  while [ ! -x /usr/local/bin/provision_ns.sh ]; do sleep 1; done; /usr/local/bin/provision_ns.sh
-
-  maybe_postprovision
-  finish
-}
-
-skip_provision() {
-  log "Provisioning done in a previous step, skipping..."
-  log "Complete!  Move on to the next step."
+  fi
 }
 
 # Main stuff
+mkdir -p /provision /etc/nomad.d /opt/nomad/data
 
-if [ ! -f /provision_complete ]; then provision; else skip_provision; fi
+maybe_provision_base
+maybe_preprovision
+maybe_provision_namespace
+maybe_provision
+maybe_postprovision
+finish
