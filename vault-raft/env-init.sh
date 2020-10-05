5b281a1f6c66768e2fb2562bcefe0ddec0bf7bb7
Yoko Hyakuna <yoko@hashicorp.com>
2020-10-05 15:56:10 -0700

    Removed unnecessary export

--- vault-raft/env-init.sh
+++ vault-raft/env-init.sh
@@ -5,9 +5,6 @@ chmod +x ~/.bin/unzip
 curl -s -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.5.4/vault_1.5.4_linux_amd64.zip &&
 unzip -d ~/.bin ~/vault.zip && rm ~/vault.zip
 
-export VAULT_ADDR='http://127.0.0.1:8200'
-export VAULT_AGENT_ADDR='http://127.0.0.1:8007'
-
 apt-get install -y jq
 
-ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.5.4/vault_1.5.4_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip && export VAULT_ADDR='http://127.0.0.1:8200' && export VAULT_AGENT_ADDR='http://127.0.0.1:8007' && apt-get install -y jq  &"
+ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.5.4/vault_1.5.4_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip && apt-get install -y jq  &"
