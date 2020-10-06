1117b4384ac5a36c599354f3cfc4af1197acc3f3
Lynn Frank <franklin.webber@gmail.com>
2020-10-06 12:33:31 -0500

    Replaces the service with a nohup

--- vault-pki-engine/step0.setup.sh
+++ vault-pki-engine/step0.setup.sh
@@ -1,5 +1,6 @@
 # Start the Vault server in the background
-/usr/bin/vault server -dev -dev-root-token-id=root -dev-listen-address=0.0.0.0:8200 &
+mkdir -p ~/log
+nohup sh -c "vault server -dev -dev-root-token-id="root" -dev-listen-address=0.0.0.0:8200 >~/log/vault.log 2>&1" > ~/log/nohup.log &/usr/bin/vault server -dev -dev-root-token-id=root -dev-listen-address=0.0.0.0:8200 &
 
 sleep 5
 
