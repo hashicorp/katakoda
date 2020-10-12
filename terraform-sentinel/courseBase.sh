477906b2430ad53f64a8e02c2bd7fdce25f4f73c
Rachel Sharp <rachel@hashicorp.com>
2020-10-12 00:32:11 -0400

    Update courseBase.sh

--- terraform-sentinel/courseBase.sh
+++ terraform-sentinel/courseBase.sh
@@ -1,8 +1,6 @@
 mkdir -p ~/terraform-sentinel
 git clone https://github.com/hashicorp/learn-terraform-sentinel.git ~/terraform-sentinel
 
-mv ~/restrict-s3-buckets.sentinel ~/terraform-sentinel
-
 cd ~/terraform-sentinel
 
 clear
