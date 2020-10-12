78fa63ce91d59906d56d05db5a6eb4322c0de9da
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2020-10-11 21:45:18 -0400

    postprovision in code.sh

--- securing-nomad/enable-acls/provision.code.sh
+++ securing-nomad/enable-acls/provision.code.sh
@@ -1,1 +1,1 @@
-rm -f /provision_complete; while [ ! -x /usr/local/bin/provision.sh ]; do sleep 1; done; /usr/local/bin/provision.sh; source ~/tls_environment
+rm -f /provision_complete; while [ ! -x /usr/local/bin/provision.sh ]; do sleep 1; done; /usr/local/bin/provision.sh; /usr/local/bin/provision_scenario_post.sh; source ~/tls_environment
