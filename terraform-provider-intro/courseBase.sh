6d07545c52bf5afc9cee06bba147edd184f0b4ec
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-12-09 20:31:10 -0800

    Dont need to rename binary

--- terraform-provider-intro/courseBase.sh
+++ terraform-provider-intro/courseBase.sh
@@ -12,8 +12,7 @@ cd ~/learn-terraform-hashicups-provider/docker_compose
 mkdir -p ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64
 curl -LO https://github.com/hashicorp/terraform-provider-hashicups/releases/download/v0.3.1/terraform-provider-hashicups_0.3.1_linux_amd64.zip
 unzip terraform-provider-hashicups_0.3.1_linux_amd64.zip -d ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64
-mv ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64/terraform-provider-hashicups ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64/terraform-provider-hashicups
-chmod +x ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64/terraform-provider-hashicups
+chmod +x ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64/terraform-provider-hashicups_v0.3.1
 
 # Prevent `yes` command from accidentally being run
 alias yes=""
