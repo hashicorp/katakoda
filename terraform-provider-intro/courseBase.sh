1eb217e8cd49768e13298f87f0e3ca917f14c758
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-12-09 21:14:26 -0800

    Update to provider intro to TF 0.14, Hashicups 0.3.1 (#298)
    * Update to provider intro to TF 0.14, Hashicups 0.3.1
    
    * Update to provider intro to TF 0.14, Hashicups 0.3.1
    
    * Dont need to rename binary

--- terraform-provider-intro/courseBase.sh
+++ terraform-provider-intro/courseBase.sh
@@ -1,18 +1,18 @@
 # Install Terraform and init config
 
-# Installs Terraform 0.13
-curl -O https://releases.hashicorp.com/terraform/0.13.0-rc1/terraform_0.13.0-rc1_linux_amd64.zip
-unzip terraform_0.13.0-rc1_linux_amd64.zip -d /usr/local/bin/
+# Installs Terraform 0.14
+curl -O https://releases.hashicorp.com/terraform/0.14.2/terraform_0.14.2_linux_amd64.zip
+unzip terraform_0.14.2_linux_amd64.zip -d /usr/local/bin/
 
 # Clone docker compose files
 git clone -b katacoda https://github.com/hashicorp/learn-terraform-hashicups-provider
 cd ~/learn-terraform-hashicups-provider/docker_compose
 
 # Install HashiCups provider 
-curl -Lo terraform-provider-hashicups https://github.com/hashicorp/terraform-provider-hashicups/releases/download/v0.2.0/terraform-provider-hashicups_v0.2.0_linux_amd64.0
-chmod +x terraform-provider-hashicups
-mkdir -p ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.2.0/linux_amd64
-mv terraform-provider-hashicups ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.2.0/linux_amd64
+mkdir -p ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64
+curl -LO https://github.com/hashicorp/terraform-provider-hashicups/releases/download/v0.3.1/terraform-provider-hashicups_0.3.1_linux_amd64.zip
+unzip terraform-provider-hashicups_0.3.1_linux_amd64.zip -d ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64
+chmod +x ~/.terraform.d/plugins/hashicorp.com/edu/hashicups/0.3.1/linux_amd64/terraform-provider-hashicups_v0.3.1
 
 # Prevent `yes` command from accidentally being run
 alias yes=""
